ea94124ce19d7ec80da52fd83392c2ac2ef59f88
Chen Zekai <zachstarkk@gmail.com>
2024-06-09 20:59:21 -0400

    fix llama3 formatting template

--- llama3med/data/template/llama3_template.py
+++ llama3med/data/template/llama3_template.py
@@ -1,16 +1,11 @@
+import copy
+import re
 from dataclasses import dataclass
-
-import tokenizers
-from packaging import version
 
 from ...utils.constants import *
 from . import register_template
 from .base import Template
 from .formatter import EmptyFormatter, Formatter, StringFormatter
-
-IS_TOKENIZER_GREATER_THAN_0_14 = version.parse(tokenizers.__version__) >= version.parse(
-    "0.14"
-)
 
 system = "You are a helpful language and vision assistant. "
 "You are able to understand the visual content that the user provides, "
@@ -22,41 +17,80 @@ system = "You are a helpful language and vision assistant. "
 class Llama3Template(Template):
     format_image_token: "Formatter" = StringFormatter(slot="<image>\n{{content}}")
     format_user: "Formatter" = StringFormatter(
-        slot="<|start_header_id|>user<|end_header_id|>\n\n" + "{{content}}" + "<|eot_id|>"
+        slot="<|start_header_id|>user<|end_header_id|>\n\n"
+        + "{{content}}"
+        + "<|eot_id|>"
     )
     format_assistant: "Formatter" = StringFormatter(
-        slot="<|start_header_id|>assistant<|end_header_id|>\n\n" + "{{content}}" + "<|eot_id|>"
+        slot="<|start_header_id|>assistant<|end_header_id|>\n\n"
+        + "{{content}}"
+        + "<|eot_id|>"
     )
-    system: "Formatter" = EmptyFormatter(slot="<|start_header_id|>system<|end_header_id|>\n\n" + system + "<|eot_id|>")
-    separator: "Formatter" = EmptyFormatter(slot=["<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n", "<|eot_id|>"])
+    system: "Formatter" = EmptyFormatter(
+        slot="<|start_header_id|>system<|end_header_id|>\n\n" + system + "<|eot_id|>"
+    )
+    separator: "Formatter" = EmptyFormatter(
+        slot=["<|start_header_id|>assistant<|end_header_id|>\n\n", "<|eot_id|>"]
+    )
 
-    def _make_masks(self, labels, tokenizer, sep, eos_token_length, rounds):
+    def _make_masks(self, labels, tokenizer, sep, rounds):
         cur_len = 1  # bos
-        eos_token_length = 1
         bos_token_length = 1
         labels[:cur_len] = IGNORE_INDEX
         for i, rou in enumerate(rounds):
             if rou == "":
                 break
             parts = rou.split(sep)
-            if len(parts) != 2:
-                break
-            parts[0] += sep
+            if len(parts) == 2:
+                parts[0] += sep
             round_len = (
-                len(self.tokenizer_image_token(rou, tokenizer))
-                + eos_token_length
-                - bos_token_length
+                len(self.tokenizer_image_token(rou, tokenizer)) - bos_token_length
             )
             instruction_len = (
-                len(self.tokenizer_image_token(parts[0], tokenizer))
-                - 1
-                - bos_token_length
+                len(self.tokenizer_image_token(parts[0], tokenizer)) - bos_token_length
             )
-            if i != 0 and not tokenizer.legacy and IS_TOKENIZER_GREATER_THAN_0_14:
-                round_len -= 1
-                instruction_len -= 1
+
             labels[cur_len : cur_len + instruction_len] = IGNORE_INDEX
             cur_len += round_len
 
         labels[cur_len:] = IGNORE_INDEX
         return labels, cur_len
+
+    def make_labels(self, input_ids, prompt, tokenizer):
+        def separate_rounds(prompt):
+            # Define a regular expression pattern to split the sequence
+            parts = re.split(
+                r"(<\|start_header_id\|>system<\|end_header_id\|>\n\n|<\|start_header_id\|>user<\|end_header_id\|>\n\n|<\|start_header_id\|>assistant<\|end_header_id\|>\n\n)",
+                prompt,
+            )
+
+            # Reattach the split delimiters to the following text
+            result = []
+            for i in range(1, len(parts), 2):
+                result.append(parts[i] + parts[i + 1].strip())
+
+            return result
+
+        labels = copy.deepcopy(input_ids)
+        sep, eos_token = self.separator.apply()
+        total_len = int(labels.ne(tokenizer.pad_token_id).sum())
+        if tokenizer.pad_token_id == tokenizer.eos_token_id:
+            total_len += prompt.count(eos_token)
+        rounds = separate_rounds(prompt)
+        labels, cur_len = self._make_masks(labels, tokenizer, sep, rounds)
+        if cur_len < tokenizer.model_max_length:
+            # import time
+
+            if cur_len != total_len:
+                print(
+                    f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
+                    f" (ignored)"
+                )
+                # print("number of rounds: ", len(rounds) - 1)
+                # print("rounds: ", rounds[:-1])
+                # print("prompt: ", prompt)
+                # print(labels)
+                # print(input_ids)
+                # time.sleep(5)
+                # labels[:] = IGNORE_INDEX
+        return labels
--- llama3med/data/template/test_tokenizer.py
+++ llama3med/data/template/test_tokenizer.py
@@ -1,8 +1,33 @@
+import copy
+import re
+
+import tokenizers
 import torch
+from packaging import version
 from transformers import AutoTokenizer
 
+IGNORE_INDEX = 128003
 IMAGE_TOKEN_INDEX = 128002
 DEFAULT_IMAGE_TOKEN = "<|reserved_special_token_0|>"
+IS_TOKENIZER_GREATER_THAN_0_14 = version.parse(tokenizers.__version__) >= version.parse(
+    "0.14"
+)
+
+
+
+def separate_rounds(prompt):
+    # Define a regular expression pattern to split the sequence
+    parts = re.split(
+        r"(<\|start_header_id\|>system<\|end_header_id\|>\n\n|<\|start_header_id\|>user<\|end_header_id\|>\n\n|<\|start_header_id\|>assistant<\|end_header_id\|>\n\n)",
+        prompt,
+    )
+
+    # Reattach the split delimiters to the following text
+    result = []
+    for i in range(1, len(parts), 2):
+        result.append(parts[i] + parts[i + 1].strip())
+
+    return result
 
 
 def tokenizer_image_token(
@@ -35,9 +60,41 @@ def tokenizer_image_token(
     return input_ids
 
 
+def make_masks(labels, tokenizer, sep, eos_token_length, rounds):
+    cur_len = 1  # bos
+    eos_token_length = 1
+    bos_token_length = 1
+    labels[:cur_len] = IGNORE_INDEX
+    for i, rou in enumerate(rounds):
+        if rou == "":
+            break
+        parts = rou.split(sep)
+        if len(parts) == 2:
+            parts[0] += sep
+        round_len = (
+            len(tokenizer_image_token(rou, tokenizer))
+            - bos_token_length
+        )
+        instruction_len = (
+            len(tokenizer_image_token(parts[0], tokenizer)) - bos_token_length
+        )
+
+        labels[cur_len : cur_len + instruction_len] = IGNORE_INDEX
+        cur_len += round_len
+
+    labels[cur_len:] = IGNORE_INDEX
+    print(cur_len)
+    return labels, cur_len
+
+
 model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer.pad_token = tokenizer.eos_token
+# print(tokenizer("<|start_header_id|>system<|end_header_id|>\n\nYou are a pirate chatbot who always responds in pirate speak!"))
+# print(tokenizer.bos_token)
+# print(tokenizer.eos_token)
+# tokenizer.eos_token = ""
 # model = AutoModelForCausalLM.from_pretrained(
 #     model_id,
 #     torch_dtype=torch.bfloat16,
@@ -51,10 +108,17 @@ messages = [
         "role": "system",
         "content": "You are a pirate chatbot who always responds in pirate speak!",
     },
-    {"role": "user", "content": "<|reserved_special_token_0|>\nWhat is of this image?"},
-    {"role": "assistant", "content": "It's a dog"},
-    {"role": "user", "content": "<|reserved_special_token_0|>\nHow's the dog look like?"},
-    {"role": "assistant", "content": "It's small."},
+    {"role": "user", "content": "What is of this image?"},
+    # {"role": "assistant", "content": "It's a dog"},
+    {
+        "role": "user",
+        "content": "How's the dog look like?",
+    },
+    # {"role": "assistant", "content": "ok."},
+    {
+        "role": "user",
+        "content": "How does the dog look feel?",
+    },
 ]
 
 prompt = tokenizer.apply_chat_template(
@@ -63,9 +127,30 @@ prompt = tokenizer.apply_chat_template(
     return_tensors="pt",
     tokenize=False,
 )
+prompt = "".join(prompt.split(tokenizer.bos_token)[1])
+# print([prompt])
+# prompt = "<|start_header_id|>system<|end_header_id|>\n\nYou are a pirate chatbot who always responds in pirate speak!<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n<|reserved_special_token_0|>\nWhat is of this image?<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\nIt's a dog<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n<|reserved_special_token_0|>\nHow's the dog look like?<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\nIt's small.<|eot_id|>"
+# print(tokenizer(prompt))
+# print([prompt])
 
-print(tokenizer_image_token(prompt, tokenizer))
-print([tokenizer.decode(tokenizer_image_token(prompt, tokenizer))])
+input_ids = tokenizer_image_token(prompt, tokenizer, return_tensors="pt")
+print(input_ids)
+
+labels = copy.deepcopy(input_ids)
+sep, eos_token = ("<|start_header_id|>assistant<|end_header_id|>\n\n", "<|eot_id|>")
+total_len = int(labels.ne(tokenizer.pad_token_id).sum())
+if tokenizer.pad_token_id == tokenizer.eos_token_id:
+    total_len += prompt.count(eos_token)
+# rounds = prompt.split(sep)
+print(total_len)
+rounds = separate_rounds(prompt)
+# print(rounds)
+# eos_token_length = len(tokenizer.encode(eos_token))
+
+masks = make_masks(labels, tokenizer, sep=sep, eos_token_length=1, rounds=rounds)
+# print([tokenizer.decode(masks[0])])
+print(masks)
+# print([tokenizer.decode(tokenizer_image_token(prompt, tokenizer))])
 # print([input_ids])
 
 # print([tokenizer.decode([128000, 128006,   9125, 128007,    271,   2675,    527,    264,  55066,
